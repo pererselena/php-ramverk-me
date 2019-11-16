39dc99e773872de9c8704da9368d39830ca90aca
Elena Perers <elena.perers@chasacademy.se>
2019-11-16 15:55:47 +0100

    Test for Json controller

---
+++ test/Controller/IpVerifyJsonControllerTest.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Anax\Controller;
+
+use Anax\DI\DIFactoryConfig;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the IpVerifyJsonController.
+ */
+class IpVerifyJsonControllerTest extends TestCase
+{
+    
+    // Create the di container.
+    protected $di;
+    protected $controller;
+
+
+
+    /**
+     * Prepare before each test.
+     */
+    protected function setUp()
+    {
+        global $di;
+
+        // Setup di
+        $this->di = new DIFactoryConfig();
+        $this->di->loadServices(ANAX_INSTALL_PATH . "/config/di");
+
+        // Use a different cache dir for unit test
+        $this->di->get("cache")->setPath(ANAX_INSTALL_PATH . "/test/cache");
+
+        // View helpers uses the global $di so it needs its value
+        $di = $this->di;
+
+        // Setup the controller
+        $this->controller = new IpVerifyJsonController();
+        $this->controller->setDI($this->di);
+        $this->controller->initialize();
+    }
+
+
+
+    /**
+     * Test the route "index".
+     */
+    public function testIndexAction()
+    {
+        $request = $this->di->get("request");
+        //$response = $this->di->get("response");
+        //$request->setGet("ip", "8.8.8.8");
+        $res = $this->controller->indexActionGet();
+        $this->assertIsArray($res);
+
+        $json = $res[0];
+        $this->assertFalse($json["isValid"]);
+    }
+
+    /**
+     * Test the route "index".
+     */
+    public function testIndexActionWithIp()
+    {
+        $request = $this->di->get("request");
+        //$response = $this->di->get("response");
+        $request->setGet("ip", "8.8.8.8");
+        $res = $this->controller->indexActionGet();
+        $this->assertIsArray($res);
+
+        $json = $res[0];
+        $exp = "8.8.8.8";
+        $this->assertContains($exp, $json["ip"]);
+    }
+}
