47034ef48da89045fad580d4ea2241e93df0c5c2
Elena Perers <elena.perers@chasacademy.se>
2019-11-21 15:06:54 +0100

    Added link to view

--- view/ipGeo/index.php
+++ view/ipGeo/index.php
@@ -21,8 +21,8 @@ namespace Anax\View;
         <input class="buttons_input" type="submit" value="Validera" />
     </p>
 </form>
-<a href="json_ip/?ip=8.8.8.8">Test API rätt svar</a>
-<a href="json_ip/?ip=8.8.8">Test API fel svar</a>
+<a href="json_geo/?ip=8.8.8.8">Test API rätt svar</a>
+<a href="json_geo/?ip=8.8.8">Test API fel svar</a>
 <p>För att använda APIt använd följande URL: http://www.student.bth.se/~elpr18/dbwebb-kurser/ramverk1/me/redovisa/htdocs/json_ip/?ip=8.8.8.8 där det efter ip?= byts ut mot den IP address man vill validera.</p>
 <?php if ($geoInfo) : ?>
     <div>
@@ -36,6 +36,6 @@ namespace Anax\View;
         <p>Flagga: <img src="<?= $geoInfo["flag"]; ?>" width="40" height="30" alt="Flagga" /> </p>
         <!-- <p>Karta: <?= $geoInfo["map"]; ?></p>
         <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=17.504696846008304%2C59.55902719842611%2C17.53379344940186%2C59.58580401594082&amp;layer=mapnik&amp;marker=59.572418270439364%2C17.519245147705078" style="border: 1px solid black"></iframe><br /><small><a href="https://www.openstreetmap.org/?mlat=59.5724&amp;mlon=17.5192#map=15/59.5724/17.5192">Visa större karta</a></small> -->
-        <a href="json_ip/?ip=<?= $geoInfo["ip"]; ?>">API svar</a>
+        <a href="json_geo/?ip=<?= $geoInfo["ip"]; ?>">API svar</a>
     </div>
 <?php endif ?>
