8b0fbb36c3c37bc430d3ada4f54d5ade62df8ef6
Elena Perers <elena.perers@chasacademy.se>
2019-11-21 15:06:00 +0100

    Mounted json url

---
+++ config/router/303_ip-geo-json-controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Load the sample json controller.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Ip geo Json Controller.",
+            "mount" => "json_geo",
+            "handler" => "\Anax\Controller\IpGeoJsonController",
+        ],
+    ]
+];
