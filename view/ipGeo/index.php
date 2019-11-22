0658922f6c297cfb161d3dc3da4d8259ec30c036
Elena Perers <elena.perers@chasacademy.se>
2019-11-22 18:27:54 +0100

    Updated doc for API

--- view/ipGeo/index.php
+++ view/ipGeo/index.php
@@ -23,9 +23,29 @@ namespace Anax\View;
 </form>
 <a href="json_geo/?ip=8.8.8.8">Test API rätt svar</a>
 <a href="json_geo/?ip=8.8.8">Test API fel svar</a>
-<p>För att använda APIt använd följande URL: http://www.student.bth.se/~elpr18/dbwebb-kurser/ramverk1/me/redovisa/htdocs/json_ip/?ip=8.8.8.8 där det efter ip?= byts ut mot den IP address man vill validera.</p>
+<p>För att använda APIt använd följande URL: http://www.student.bth.se/~elpr18/dbwebb-kurser/ramverk1/me/redovisa/htdocs/json_geo/?ip=8.8.8.8 där det efter ip?= byts ut mot den IP address man vill validera.</p>
+<p>Använd metod GET och svaret kan se ut på det här sättet:</p>
+<pre>
+    {
+        "geoInfo": {
+            "ip": "8.8.8.8",
+            "protocol": "IPV4",
+            "isValid": true,
+            "domain": "dns.google",
+            "long": -122.07540893554688,
+            "lat": 37.419158935546875,
+            "country": "United States",
+            "city": "Mountain View",
+            "map": "https://www.openstreetmap.org/#map=15/37.419158935547/-122.07540893555",
+            "flag": "http://assets.ipstack.com/flags/us.svg",
+            "embed": "-122.06089893555%2C37.432538935547%2C-122.08991893555%2C37.405778935547"
+        },
+        "ip": "8.8.8.8"
+    }
+</pre>
 <?php if ($geoInfo) : ?>
     <div>
+        <a href="json_geo/?ip=<?= $geoInfo["ip"]; ?>">API svar</a>
         <p>Ip: <?= $geoInfo["ip"]; ?></p>
         <p>Giltig ip adress: <?= $geoInfo["isValid"] ? "Godkänd" : "Ej godkänd"; ?></p>
         <p>Protokoll: <?= $geoInfo["protocol"]; ?></p>
@@ -37,6 +57,5 @@ namespace Anax\View;
         <div>
             <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=<?= $geoInfo["embed"]; ?>&amp;layer=mapnik&amp;marker=<?= $geoInfo["lat"]; ?>%2C<?= $geoInfo["long"]; ?>" style="border: 1px solid black"></iframe><br /><small><a href="https://www.openstreetmap.org/?mlat=<?= $geoInfo["lat"]; ?>&amp;mlon=<?= $geoInfo["long"]; ?>#map=15/<?= $geoInfo["lat"]; ?>/<?= $geoInfo["long"]; ?>">Visa större karta</a></small>
         </div>
-        <a href="json_geo/?ip=<?= $geoInfo["ip"]; ?>">API svar</a>
     </div>
 <?php endif ?>
