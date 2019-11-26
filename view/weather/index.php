e738dad0526ee04d0fff640d17b957e176d34a89
Elena Perers <elena.perers@chasacademy.se>
2019-11-26 23:21:08 +0100

    Created radio buttons for search

--- view/weather/index.php
+++ view/weather/index.php
@@ -11,6 +11,12 @@ namespace Anax\View;
     <br>
     <input type="text" name="city" placeholder="Ort" />
     <br>
+    <input type="radio" name="search_type" value="history" />
+    <br>
+    <input type="radio" name="search_type" value="forecast" />
+    <br>
+    <input type="radio" name="search_type" value="currently" />
+    <br>
     <p>
         <input class="buttons_input" type="submit" name="weather" value="Söka" />
     </p>
@@ -19,13 +25,6 @@ namespace Anax\View;
     <div>
         <a href="json_geo/?ip=<?= $ip; ?>">API svar</a>
         <p>Ip: <?= $ip; ?></p>
-        <p>Väder idag: <?= $weather["currently"]["summary"]; ?></p>
-        <p>Temperatur idag: <?= $weather["currently"]["temperature"]; ?></p>
-        <p>Vindhastighet idag: <?= $weather["currently"]["windSpeed"]; ?></p>
-        <p>Uv-index idag: <?= $weather["currently"]["uvIndex"]; ?></p>
-        <p>Geografisk position: <?= $weather["lat"]; ?>, <?= $weather["long"]; ?></p>
-        <p>Ort: <?= $weather["city"]; ?></p>
-
         <div>
             <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=<?= $weather["embed"]; ?>&amp;layer=mapnik&amp;marker=<?= $weather["lat"]; ?>%2C<?= $weather["long"]; ?>" style="border: 1px solid black"></iframe><br /><small><a href="https://www.openstreetmap.org/?mlat=<?= $weather["lat"]; ?>&amp;mlon=<?= $weather["long"]; ?>#map=15/<?= $weather["lat"]; ?>/<?= $weather["long"]; ?>">Visa större karta</a></small>
         </div>
