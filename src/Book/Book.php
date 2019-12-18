6ce704d3a0017804aa6cacc9c732210f6dc2b305
Elena Perers <elena.perers@chasacademy.se>
2019-12-18 15:25:19 +0100

    Scaffolded and organized code for Book

---
+++ src/Book/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Anax\Book;
+
+use Anax\DatabaseActiveRecord\ActiveRecordModel;
+
+/**
+ * A database driven model using the Active Record design pattern.
+ */
+class Book extends ActiveRecordModel
+{
+    /**
+     * @var string $tableName name of the database table.
+     */
+    protected $tableName = "Book";
+
+
+
+    /**
+     * Columns in the table.
+     *
+     * @var integer $id primary key auto incremented.
+     */
+    public $id;
+    public $column1;
+    public $column2;
+}
---
+++ src/Book/HTMLForm/CreateForm.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Anax\Book\HTMLForm;
+
+use Anax\HTMLForm\FormModel;
+use Psr\Container\ContainerInterface;
+use Anax\Book\Book;
+
+/**
+ * Form to create an item.
+ */
+class CreateForm extends FormModel
+{
+    /**
+     * Constructor injects with DI container.
+     *
+     * @param Psr\Container\ContainerInterface $di a service container
+     */
+    public function __construct(ContainerInterface $di)
+    {
+        parent::__construct($di);
+        $this->form->create(
+            [
+                "id" => __CLASS__,
+                "legend" => "Details of the item",
+            ],
+            [
+                "column1" => [
+                    "type" => "text",
+                    "validation" => ["not_empty"],
+                ],
+                        
+                "column2" => [
+                    "type" => "text",
+                    "validation" => ["not_empty"],
+                ],
+
+                "submit" => [
+                    "type" => "submit",
+                    "value" => "Create item",
+                    "callback" => [$this, "callbackSubmit"]
+                ],
+            ]
+        );
+    }
+
+
+
+    /**
+     * Callback for submit-button which should return true if it could
+     * carry out its work and false if something failed.
+     *
+     * @return bool true if okey, false if something went wrong.
+     */
+    public function callbackSubmit() : bool
+    {
+        $book = new Book();
+        $book->setDb($this->di->get("dbqb"));
+        $book->column1  = $this->form->value("column1");
+        $book->column2 = $this->form->value("column2");
+        $book->save();
+        return true;
+    }
+
+
+
+    /**
+     * Callback what to do if the form was successfully submitted, this
+     * happen when the submit callback method returns true. This method
+     * can/should be implemented by the subclass for a different behaviour.
+     */
+    public function callbackSuccess()
+    {
+        $this->di->get("response")->redirect("book")->send();
+    }
+
+
+
+    // /**
+    //  * Callback what to do if the form was unsuccessfully submitted, this
+    //  * happen when the submit callback method returns false or if validation
+    //  * fails. This method can/should be implemented by the subclass for a
+    //  * different behaviour.
+    //  */
+    // public function callbackFail()
+    // {
+    //     $this->di->get("response")->redirectSelf()->send();
+    // }
+}
